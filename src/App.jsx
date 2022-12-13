965905f243438170ab9e6666452b9690059ca86f
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-12-13 14:05:49 +0530

    Navbar Designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react';
+import Navbar from './Navbar';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <Navbar></Navbar>
      
     </div>
   )
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'
+
+
+export default  function Navbar(){
+  return(
+    <>
+    <nav>
+        <img src="../public/Fill 213.png" alt="image-logo" />
+        <h1>Personal Journal</h1>
+    </nav>
+    </>
+  )
+}
