a19180e9f9380dff8a4a2aa4adeb312f7dcc0e8a
hey <89584874+Letsbuilditnow@users.noreply.github.com>
2022-12-15 22:24:15 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -16,5 +16,14 @@ export default [
         endDate: "8 June, 2021",
         description:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
         imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        title:"Taj Mahal",
+        location:"India",
+        googleMapsUrl:"https://goo.gl/maps/bL4EH3xMpUwn2Rry8",
+        startDate:"27 June 2021",
+        endDate:"8 August 2021",
+        description:`The Taj Mahal was designated as a UNESCO World Heritage Site in 1983 for being "the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage". It is regarded by many as the best example of Mughal architecture and a symbol of India's rich history.`,
+        imageUrl:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.PBVpd65UNCkcEKEFRcScJwHaF6%26pid%3DApi&f=1&ipt=ae5cb32a4968351d4a61414b6af86ea5a750919c8e4cb1131ded393689339a7c&ipo=images"
     }
 ]
